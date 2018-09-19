027c17006c3ae33164ce58f3d5197787df855c2d
isaac-utilizecore <43425407+isaac-utilizecore@users.noreply.github.com>
2018-09-19 19:40:59 -0400

    all the scripts

---
+++ resources/story.js
@@ -0,0 +1,662 @@
+var story = {
+"docName": "compliancy-relationships",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"pages": [
+{
+"image": "vendors.png",
+"image2x": "vendors@2x.png",
+"width": 1440,
+"height": 1502,
+"title": "Vendors",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    112,
+    120,
+    210,
+    163
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    112,
+    120,
+    210,
+    163
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1320,
+    244,
+    1425,
+    285
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1320,
+    244,
+    1425,
+    285
+   ],
+   "page": 2
+  }
+]}
+,{
+"image": "clients.png",
+"image2x": "clients@2x.png",
+"width": 1440,
+"height": 1735,
+"title": "Clients",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    853,
+    478,
+    943,
+    519
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    853,
+    478,
+    943,
+    519
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    853,
+    768,
+    943,
+    809
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    853,
+    768,
+    943,
+    809
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    853,
+    1058,
+    943,
+    1099
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    853,
+    1058,
+    943,
+    1099
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    853,
+    1353,
+    943,
+    1394
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    853,
+    1353,
+    943,
+    1394
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    853,
+    1643,
+    943,
+    1684
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    853,
+    1643,
+    943,
+    1684
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    20,
+    120,
+    118,
+    163
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    20,
+    120,
+    118,
+    163
+   ],
+   "page": 0
+  }
+]}
+,{
+"image": "compliance_manager.png",
+"image2x": "compliance_manager@2x.png",
+"width": 1440,
+"height": 1200,
+"title": "Compliance Manager",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1199,
+    303,
+    1299,
+    343
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    1199,
+    303,
+    1299,
+    343
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    118,
+    161,
+    216,
+    204
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    118,
+    161,
+    216,
+    204
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    20,
+    161,
+    118,
+    204
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    20,
+    161,
+    118,
+    204
+   ],
+   "page": 0
+  }
+]}
+,{
+"image": "compliance_type_text.png",
+"image2x": "compliance_type_text@2x.png",
+"width": 599,
+"height": 600,
+"title": "Compliance Type Text",
+'type': 'overlay',
+'overlayShadow': 1,
+"links": [
+{
+  "rect": [
+    149,
+    179,
+    398,
+    213
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    149,
+    179,
+    398,
+    213
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    383,
+    531,
+    583,
+    571
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    383,
+    531,
+    583,
+    571
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    545,
+    0,
+    598,
+    53
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    545,
+    0,
+    598,
+    53
+   ],
+   "page": 6
+  }
+]}
+,{
+"image": "compliance_type_upload_1.png",
+"image2x": "compliance_type_upload_1@2x.png",
+"width": 599,
+"height": 600,
+"title": "Compliance Type Upload 1",
+'type': 'overlay',
+'overlayShadow': 1,
+"links": [
+{
+  "rect": [
+    398,
+    261,
+    472,
+    291
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    398,
+    261,
+    472,
+    291
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    149,
+    179,
+    398,
+    213
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    149,
+    179,
+    398,
+    213
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    383,
+    333,
+    583,
+    373
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    383,
+    333,
+    583,
+    373
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    545,
+    0,
+    598,
+    53
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    545,
+    0,
+    598,
+    53
+   ],
+   "page": 6
+  }
+]}
+,{
+"image": "compliance_type_upload_2.png",
+"image2x": "compliance_type_upload_2@2x.png",
+"width": 599,
+"height": 600,
+"title": "Compliance Type Upload 2",
+'type': 'overlay',
+'overlayShadow': 1,
+"links": [
+{
+  "rect": [
+    149,
+    179,
+    398,
+    213
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    149,
+    179,
+    398,
+    213
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    383,
+    333,
+    583,
+    373
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    383,
+    333,
+    583,
+    373
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    545,
+    0,
+    598,
+    53
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    545,
+    0,
+    598,
+    53
+   ],
+   "page": 6
+  }
+]}
+,{
+"image": "edit_complienace_terms.png",
+"image2x": "edit_complienace_terms@2x.png",
+"width": 900,
+"height": 600,
+"title": "Edit Complienace Terms",
+'type': 'overlay',
+'overlayShadow': 1,
+"links": [
+{
+  "rect": [
+    695,
+    107,
+    864,
+    137
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    695,
+    107,
+    864,
+    137
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    847,
+    0,
+    899,
+    52
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    847,
+    0,
+    899,
+    52
+   ],
+   "page": 2
+  }
+]}
+,{
+"image": "compliance_wizard_1.png",
+"image2x": "compliance_wizard_1@2x.png",
+"width": 1500,
+"height": 937,
+"title": "Compliance Wizard 1",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1083,
+    809,
+    1237,
+    862
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    1083,
+    809,
+    1237,
+    862
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    957,
+    809,
+    1084,
+    862
+   ],
+   "action": "back"
+  }
+]}
+,{
+"image": "compliance_wizard_2.png",
+"image2x": "compliance_wizard_2@2x.png",
+"width": 1455,
+"height": 1200,
+"title": "Compliance Wizard 2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1070,
+    1049,
+    1213,
+    1102
+   ],
+   "page": 9
+  }
+,{
+  "rect": [
+    1070,
+    1049,
+    1213,
+    1102
+   ],
+   "page": 9
+  }
+,{
+  "rect": [
+    243,
+    1049,
+    386,
+    1102
+   ],
+   "action": "back"
+  }
+]}
+,{
+"image": "compliance_wizard_3.png",
+"image2x": "compliance_wizard_3@2x.png",
+"width": 1455,
+"height": 800,
+"title": "Compliance Wizard 3",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1073,
+    606,
+    1213,
+    659
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    1073,
+    606,
+    1213,
+    659
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    243,
+    606,
+    383,
+    659
+   ],
+   "action": "back"
+  }
+]}
+,{
+"image": "compliance_wizard_4.png",
+"image2x": "compliance_wizard_4@2x.png",
+"width": 1455,
+"height": 800,
+"title": "Compliance Wizard 4",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1073,
+    400,
+    1213,
+    453
+   ],
+   "page": 11
+  }
+,{
+  "rect": [
+    1073,
+    400,
+    1213,
+    453
+   ],
+   "page": 11
+  }
+,{
+  "rect": [
+    243,
+    400,
+    383,
+    453
+   ],
+   "action": "back"
+  }
+]}
+,{
+"image": "compliance_wizard_5.png",
+"image2x": "compliance_wizard_5@2x.png",
+"width": 1500,
+"height": 800,
+"title": "Compliance Wizard 5",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    949,
+    629,
+    1239,
+    682
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    949,
+    629,
+    1239,
+    682
+   ],
+   "page": 1
+  }
+]}
+   ]
+,"resolutions": [2],
+"title": "Compliancy Relationships",
+"highlightLinks": false
+}
